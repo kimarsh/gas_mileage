62c90b46119918157da1a52a0b2ae5fba08bf979
Kim <cressmanka@gmail.com>
2017-05-07 18:03:27 -0500

    modified plots

--- plots over time.R
+++ plots over time.R
@@ -1,4 +1,6 @@
 # 4/20/2017 kac
+# updated 5/7/2017 to add vertical lines on the graphs
+# when my marriage stopped being long-distance
 
 library(ggplot2)
 library(dplyr)
@@ -28,7 +30,9 @@ dat <- dat %>%
            mileage = milesadded/Gallons,
            gasprice = Cost/Gallons,
            Month = as.factor(month(Date, abbr=TRUE)),
-           Year = year(Date))
+           Year = year(Date),
+           Quarter = quarter(Date, with_year=TRUE),
+           Halfyear = semester(Date, with_year=TRUE))
 # 
 # bymonth <- group_by(dat, Year, Month) %>%
 #     summarize(., avgmpg = mean(mileage, na.rm=TRUE), 
@@ -39,45 +43,88 @@ dat <- dat %>%
 pdftitle <- paste0("fillup graphs_", Sys.Date(), ".pdf")
 pdf(pdftitle, height=8, width=10)
 
-ggplot(dat) +
-    geom_point(aes(x=Date, y=Odometer)) +
-    theme_minimal() +
-    ggtitle("Odometer reading over time")
-
-ggplot(dat) +
-    geom_point(aes(x=Date, y=gasprice)) +
+# gas price
+ggplot(dat, aes(x=Date, y=gasprice)) +
+    geom_point(size=2, col="darkslategray") +
     theme_minimal() +
     ggtitle("Gas price over time") +
     ylab("Price per gallon ($)")
 
-ggplot(dat) +
-    geom_boxplot(aes(x=Month, y=gasprice)) +
+# add LOESS smoothing to that
+ggplot(dat, aes(x=Date, y=gasprice)) +
+    geom_point(size=2, col="darkslategray") +
+    geom_smooth(se=FALSE) +
     theme_minimal() +
-    ggtitle("Gas price binned by month, across all years") +
+    ggtitle("Gas price over time with LOESS smoothing") +
     ylab("Price per gallon ($)")
 
-ggplot(dat) +
-    geom_point(aes(x=Date, y=mileage)) +
+# how many days between stopping at the gas station?
+# again, red line is when marriage stopped being long-distance
+ggplot(dat, aes(x=Date, y=daysbetween)) +
+    geom_point(size=2, color="darkslategray") +
     theme_minimal() +
-    ggtitle("Mileage over time") +
-    ylab("mpg")
-
-ggplot(dat) +
-    geom_boxplot(aes(x=Month, y=mileage)) +
-    theme_minimal() +
-    ggtitle("Mileage binned by month, across all years") +
-    ylab("mpg")
-
-ggplot(dat) +
-    geom_point(aes(x=Date, y=daysbetween)) +
-    theme_minimal() +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
     ggtitle("Days between fill-ups") +
     ylab("Days")
 
-ggplot(dat) +
-    geom_boxplot(aes(x=Month, y=daysbetween)) +
+# same as above, but with loess smoothing
+ggplot(dat, aes(x=Date, y=daysbetween)) +
+    geom_point(size=2, color="darkslategray") +
     theme_minimal() +
-    ggtitle("Days between fill-ups, binned by month, across all years") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    geom_smooth(se=FALSE) +
+    ggtitle("Days between fill-ups, with LOESS smoothing") +
     ylab("Days")
+
+# box plot of days between fill ups, by quarter
+ggplot(dat, aes(x=Date, y=daysbetween)) +
+    geom_boxplot(aes(group=Quarter), fill="lightgray") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    theme_minimal() +
+    ggtitle("Days between fill-ups, by quarter") +
+    ylab("Days")
+
+# box plot of days between fill ups, by 6-months
+ggplot(dat, aes(x=Date, y=daysbetween)) +
+    geom_boxplot(aes(group=Halfyear), fill="lightgray") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    theme_minimal() +
+    ggtitle("Days between fill-ups, by 6 months") +
+    ylab("Days")
+
+# odometer readings
+ggplot(dat, aes(x=Date, y=Odometer)) +
+    geom_point() +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    theme_minimal() +
+    ggtitle("Odometer reading over time")
+
+# plot mpg, with a red line for when I got to move in with my spouse
+ggplot(dat, aes(x=Date, y=mileage)) +
+    geom_point(size=2, color="darkslategray") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    geom_smooth(se=FALSE) +
+    theme_minimal() +
+    coord_cartesian(ylim = c(20, 45)) +
+    ggtitle("Mileage over time") +
+    ylab("mpg")
+
+# box plot of mpg; red line means the same
+ggplot(dat, aes(x=Date, y=mileage)) +
+    geom_boxplot(aes(group=Quarter), fill="lightgray") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    theme_minimal() +
+    coord_cartesian(ylim = c(20, 45)) +
+    ggtitle("Mileage over time, by quarter") +
+    ylab("mpg")
+
+# box plot of mpg by 6-months
+ggplot(dat, aes(x=Date, y=mileage)) +
+    geom_boxplot(aes(group=Halfyear), fill="lightgray") +
+    geom_vline(aes(xintercept = as.numeric(as.Date("2016-07-01"))), col="red", lwd=1.5) +
+    theme_minimal() +
+    coord_cartesian(ylim = c(20, 45)) +
+    ggtitle("Mileage over time, by 6-month intervals") +
+    ylab("mpg")
 
 dev.off()
